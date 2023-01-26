9751b64b55ccc1f711ac2df9e422bb4f4cb71ec2
Luis <luiswoohoo@gmail.com>
2023-01-26 10:44:47 -0600

    Update Card component css

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,7 +5,7 @@ import { downloadImage } from '../utils'
 
 const Card = ({_id, name, prompt, photo}) => {
   return (
-    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card border border-l-rose-900'>
+    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
       <img 
         className='w-full h-auto object-cover rounded-xl'
         src={photo}
